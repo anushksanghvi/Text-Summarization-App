915f6672e2fb17cc98b1f38fdd6d6791d34935ce
Anushk <anushksanghvi@gmail.com>
2024-04-15 05:11:04 +0530

    Text-Summarization content added

---
+++ README.md
@@ -0,0 +1,40 @@
+**Extractive Text-Summarization App using NLTK, using Streamlit**
+
+**NLP:**
+It is the ability of a computer program to understand human language as it is spoken and written referred to as natural language. It is a component of artificial intelligence(AI).
+
+There are two main phases to natural language processing: 
+
+1.data preprocessing
+
+2.algorithm development
+
+
+**re (RegEx or Regular Expression):**
+This is the Python regular expression module, which is used for text cleaning/ preprocessing and pattern matching operations.
+
+
+**NLTK(Natural Language Toolkit):**
+NLTK is a popular Python library used for building Python programs that work with human language data for applying in statistical natural language processing (NLP). It contains text processing libraries for tokenization, parsing, classification, stemming, tagging and semantic reasoning.
+Specifically, the following modules/components from NLTK are imported and used:
+   - stopwords: This module provides a list of stopwords (common words like "the," "a," "is," etc.) that are often removed during text processing.
+   - word_tokenize: This function tokenizes (splits) a given text into individual words.
+   - sent_tokenize: This function tokenizes (splits) a given text into individual sentences.
+
+*Installation:*
+pip3 install nltk
+
+
+**Streamlit:**
+It is an open-source Python library that makes it easy to create and share beautiful, custom web apps for machine learning and data science. In just a few minutes you can build and deploy powerful data apps.
+
+*Installation:*
+pip3 install streamlit
+
+
+**Heapq (Heap Queue):** 
+This is a built-in Python module for implementing the heap queue algorithm (Priority Queue). In this code, it is used to select the top N sentences with the highest scores for the summary.
+
+
+**To run the file:**
+python -m streamlit run TextSummarizer.py 
---
+++ TextSummarizer.py
@@ -0,0 +1,84 @@
+import re
+import streamlit as st
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+import heapq
+
+
+def nltk_summarizer(docx):
+    stopWords = set(stopwords.words("english"))
+    words = word_tokenize(docx)
+    freqTable = {}
+
+    print(words)    # Print word tokens
+    print()
+
+    # Count word frequency
+    for word in words:
+        word = word.lower()
+        if word not in stopWords:
+            if word in freqTable:
+                freqTable[word] += 1
+            else:
+                freqTable[word] = 1
+  
+    # Normalize word frequency
+    max_freq = max(freqTable.values())
+    for word in freqTable.keys():
+        freqTable[word] = (freqTable[word]/max_freq)
+
+    # Calculate sentence scores
+    sentence_list = sent_tokenize(docx)
+    sentence_scores = {}
+    for sent in sentence_list:
+        for word in nltk.word_tokenize(sent.lower()):
+            if word in freqTable.keys():
+                if len(sent.split(' ')) < 30:      # filtering condition in order to exclude very long sentences from the scoring process.
+                    if sent not in sentence_scores.keys():
+                        sentence_scores[sent] = freqTable[word]
+                    else:
+                        sentence_scores[sent] += freqTable[word] # total number of length of words
+
+    # print(sentence_list)
+    # print()
+    print(sentence_scores)      
+
+    # Get top sentences for summary
+    summary_sentences = heapq.nlargest(8, sentence_scores, key=sentence_scores.get)
+    summary = ' '.join(summary_sentences)
+    return summary
+
+
+def main():
+    st.title("Text Summarizer App")
+    activities = ["Summarize Via Text"]
+    choice = st.sidebar.selectbox("Select Activity", activities)
+
+    if choice == 'Summarize Via Text':
+        st.subheader("Text Summary using NLTK")
+        article_text = st.text_area("Enter Text Here","Type here")
+
+        #cleaning of input text
+        article_text = re.sub(r'\\[[0-9]*\\]', ' ',article_text)
+        article_text = re.sub('[^a-zA-Z.,]', ' ',article_text)
+        article_text = re.sub(r"\b[a-zA-Z]\b",'',article_text)
+        article_text = re.sub("[A-Z]\Z",'',article_text)
+        article_text = re.sub(r'\s+', ' ', article_text)
+
+        summary_choice = "NLTK"
+        if st.button("Summarize Via Text"):
+            summary_result = nltk_summarizer(article_text)
+            
+            st.write(summary_result)
+
+            # Display lengths of original text and summary text
+            st.write("Original Text Length:", len(article_text))
+            st.write("Summary Text Length:", len(summary_result))
+
+            # st.write(word_tokenize(article_text))
+
+if __name__=='__main__':
+	main()
